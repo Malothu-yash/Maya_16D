84c884c6190658fa76bd7839813949a07bfb4673
Yashwanth <malothuyashwanth10@gmail.com>
2025-10-21 23:43:38 +0530

    Update CORS origins in main.py
    Removed CORS origins for localhost and Vercel app.

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://maya-16-d.vercel.app",
     "https://maya-16-bx0ekl6w8-malothu-yashs-projects.vercel.app",  # your vercel frontend
 ]
 
